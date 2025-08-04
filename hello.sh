10375357c2da8322edb30c11378c4c29e13c0a2f
Aagry <4heromm@gmail.com>
2025-08-04 22:00:21 +0000

    Додав скрипт з використанням змінних DATA, STUDENT_NAME і hostname

---
+++ hello.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+DATA=$(date '+%Y-%m-%d %H:%M:%S')
+STUDENT_NAME="Терещенко Олександр"
+hostname=$(hostname)
+
+echo "Привіт! Я $STUDENT_NAME, працюю на машині $hostname. Сьогодні: $DATA"
